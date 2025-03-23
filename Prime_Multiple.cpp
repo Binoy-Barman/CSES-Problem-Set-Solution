235e29ab83dcf78f134dd20d3b746b12eb4c8e46
Binoy Barman <bbinoy318@gamil.com>
2025-03-23 16:29:20 +0600

    try to solve

---
+++ Prime_Multiple.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);     
+    cin.tie(NULL); cout.tie(NULL); 
+
+    int t_case ; cin>>t_case;
+   // while(t_case)--  {             }
+
+   int  n,k; cin>>n>>k;
+   vector<int>v(k);
+   for(int i=0;i<k;i++)
+   {
+     cin>>v[i];
+   }
+
+
+}
